2dbc3571502702ee3e4058291fe3212dc682d55a
vamsi <learngitvamsi@gmail.com>
2024-03-20 15:38:33 +0800

    declaring variables using variable block

--- variable/variable.tf
+++ variable/variable.tf
@@ -1,12 +1,26 @@
 variable "test" {
-  default = 10
-  list=[1,2,3]
-  map={
-    a=10
-  }
-
+  value = 10
 }
+variable "list" {
+  value = "[1,2,3]"
+}
+variable "map" {
+   default={
+     x=10
+     y=20
+   }
+}
+
+
 output "print" {
   value = var.test
 
+}
+output "print_list" {
+  value = "list[1]"
+}
+
+output "print_map" {
+  value = "map[x]"
+  //value = "map[y]"
 }
